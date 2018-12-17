75a919849e917f4bdab4bdc05287ebbe9767577e
laurabrovkina <laurabrovkina@gmail.com>
2018-12-17 22:21:19 +1300

    remove unnecessary code

---
+++ ClientApp/src/components/CandidateList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class CandidateList extends React.PureComponent {
+    render() {
+        return <div>
+            <h1>List of Candidates</h1>
+        </div>
+    }
+}
