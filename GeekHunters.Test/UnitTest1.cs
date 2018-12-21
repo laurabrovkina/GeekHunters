57bfdf7c55c19ea13cd7f8a94888faccf63ca680
Unknown <laurabrovkina@gmail.com>
2018-12-21 13:03:02 +1300

    Add unit tests

--- GeekHunters.Test/UnitTest1.cs
+++ GeekHunters.Test/UnitTest1.cs
@@ -1,14 +1,40 @@
 using System;
 using Xunit;
+using GeekHunter.Controllers;
+using GeekHunter.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace GeekHunters.Test
+namespace GeekHunter.Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
-
+    private GeekHunterContext GetContextWithData()
+            {
+                var context = new GeekHunterContext();
+            
+                var Skill1 = new Skill { Id = 1, Name = "Singer" };
+                var Skill2 = new Skill { Id = 2, Name = "Dancer" };
+                
+                context.Skill.Add(Skill1);
+                context.Skill.Add(Skill1);
+            
+                return context;
+            }
+ 
+    [Fact]
+    public void Get_WhenCalled_ReturnsOkResult()
+            {
+            using (var context = GetContextWithData())
+            {
+                 var controller = new SkillsController(context);
+           { 
+                var okResult = controller.GetAll();
+                Assert.NotNull(okResult);
+            }
+        }
         }
     }
 }
